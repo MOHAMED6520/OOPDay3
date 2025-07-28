3a1d1c426997286118ab43cc3b4e5d7045a09183
moham <moham@DESKTOP-M8CT1H1>
2025-07-28 20:49:38 +0300

    Interface

---
+++ OOPDay3/InterfaceEX01/IMyType.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPDay3.InterfaceEX01
+{
+    internal interface IMyType
+    {
+        void MyFun();
+        
+        double Salary { get; set; }
+
+        void Print()
+        { Console.WriteLine("Default Implement Methoud [Fully Implemnted Methoud]"); 
+        }
+
+
+
+
+    }
+}
--- OOPDay3/Program.cs
+++ OOPDay3/Program.cs
@@ -1,10 +1,25 @@
-﻿namespace OOPDay3
+﻿using OOPDay3.InterfaceEX01;
+
+namespace OOPDay3
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Interface
+            ////MyType myType = new MyType();
+            ////myType.Salary = 15000;
+            ////Console.WriteLine(myType.Salary);
+            ////myType.MyFun();
+
+            //IMyType myType = new MyType();
+
+            //myType.Salary = 1032101;
+            //Console.WriteLine(myType.Salary);
+            //myType.MyFun();
+            //myType.Print(); 
+            #endregion
+
         }
     }
 }
