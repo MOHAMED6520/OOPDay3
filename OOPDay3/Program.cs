f30fd8b79086e27e72fb708440c4d85c8acef61b
moham <moham@DESKTOP-M8CT1H1>
2025-07-29 20:57:46 +0300

    Part01

--- OOPDay3/Program.cs
+++ OOPDay3/Program.cs
@@ -4,7 +4,33 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
+
+            #region Part01
+
+            //Q01 b) To define a blueprint for a class
+
+            //Q02  Nothing All access modifier is valid
+
+            //Q03  b) No
+
+            //Q04  b) Yes, interfaces can inherit from multiple interfaces
+
+            //Q05  Nothing Of Options is correct !!  Correct answer is (:) 
+
+            //Q06  a) Yes
+
+            //Q07 b) No, all members are implicitly public
+
+            //Q08 b) To provide a clear separation between interface and class members
+
+            //Q09 b) No, interfaces cannot have constructors
+
+            //Q10 c) By separating interface names with commas 
+            #endregion
+
+
+
         }
     }
 }
